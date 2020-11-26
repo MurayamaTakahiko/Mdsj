ed24a8a1f9fb6711f4b3bfc69a515a1075bdb6df
NaokiTakahara <nasioyodo8986@yahoo.co.jp>
2020-11-26 17:24:16 +0900

    未来の計画額も制御

--- 183_新受注済み案件管理/js/pastDisable.js
+++ 183_新受注済み案件管理/js/pastDisable.js
@@ -21,6 +21,7 @@ jQuery.noConflict();
             row.value.交通費.disabled = true;
           }else if (moment(sellDate).isAfter(moment(), 'month')) {
             //来月以降は編集可能に
+            row.value.計画額.disabled = true;
           }else{
             //今月分は一部編集可能に
             row.value.売上月.disabled = true;

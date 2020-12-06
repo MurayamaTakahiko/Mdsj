2da7d26c9a2d069a00ae142688d26a39fe759dc6
NaokiTakahara <nasioyodo8986@yahoo.co.jp>
2020-12-06 17:52:41 +0900

    細かいレイアウト修正

--- 253_経営のコックピット/js/customer_sales_runking.js
+++ 253_経営のコックピット/js/customer_sales_runking.js
@@ -19,10 +19,10 @@ jQuery.noConflict();
     if (e.viewType !== "custom") {
       return;
     }
-    if (e.viewId != emxasConf.getConfig('VIEW_PRD_PERHOUR_PROFIT')) {
+    if (e.viewId != emxasConf.getConfig('VIEW_CST_SALES_RUNK')) {
       return;
     }
-    console.log("------perhour");
+    console.log("------cstrunk");
 
     spinner.showSpinner();
     // 画面構成に必要なdivを用意
@@ -58,9 +58,9 @@ jQuery.noConflict();
 
   let gridVal = {};
   // グリッド幅
-  gridVal.GRID_WIDTH = '1256';
+  gridVal.GRID_WIDTH = 'auto';
   // 通常のセル幅（CKP）
-  gridVal.COL_WIDTH_CKP = [90, 90, 90, 75, 75, 90, 75, 75, 90, 75, 90, 75, 75, 75, 75, 75, 75, 90, 90];
+  gridVal.COL_WIDTH_CKP = [60, 90, 90, 75, 75, 90, 90, 90, 90, 75, 75, 75, 75, 75, 75, 75, 100, 75, 75];
   // 選択したタブのセル幅保持（初期値：顧客別）
   gridVal.COL_WIDTH = gridVal.COL_WIDTH_CKP;
   // ヘッダ1段目
@@ -68,65 +68,64 @@ jQuery.noConflict();
     '<rowspan type="top"> </rowspan>',
     '<rowspan type="top"> </rowspan>',
     '<rowspan type="top"> </rowspan>',
-    '<colspan type="pre" cnt="3">販売金額</colspan>',
-    '<colspan type="pre" cnt="3">材料金額</colspan>',
-    '<colspan type="pre" cnt="3">材料ロス</colspan>',
-    '<colspan type="pre" cnt="2">製品原価</colspan>',
-    '<colspan type="pre" cnt="2">製品歩留</colspan>',
-    '<colspan type="pre" cnt="3">スプレッド</colspan>',
+    '<colspan type="pre" cnt="2">販売量</colspan>',
+    '<colspan type="pre" cnt="4">損益(千円)</colspan>',
+    '<colspan type="pre" cnt="2">構成比</colspan>',
+    '<colspan type="pre" cnt="5">参考指標</colspan>',
+    '<colspan type="pre" cnt="3">検討事項</colspan>',
   ];
   // ヘッダ2段目
   gridVal.GRID_COL_HEADERS = [
-    '<rowspan type="top">商品名</rowspan>',
-    '<rowspan type="top">製品<br>サイズ</rowspan>',
-    '<rowspan type="top">h当たり<br>生産量(kg)</rowspan>',
-    '<colspan type="top">販売重量</colspan>',
-    '<colspan type="top">販売単価</colspan>',
-    '<colspan type="top">販売金額</colspan>',
-    '<colspan type="top">母材重量</colspan>',
-    '<colspan type="top">母材単価</colspan>',
-    '<colspan type="top">母材金額</colspan>',
-    '<colspan type="top">実ﾛｽ重量</colspan>',
-    '<colspan type="top">実ﾛｽ金額</colspan>',
-    '<colspan type="top">実ﾛｽ率</colspan>',
-    '<colspan type="top">製品重量</colspan>',
-    '<colspan type="top">製品単価</colspan>',
-    '<colspan type="top">製品歩留<br>(理論)</colspan>',
-    '<colspan type="top">実際歩留</colspan>',
-    '<colspan type="top">金額</colspan>',
-    '<colspan type="top">％</colspan>',
-    '<colspan type="top">時間<br>当たり</colspan>',
+    '<rowspan type="top">ランク</rowspan>',
+    '<rowspan type="top">得意先名称</rowspan>',
+    '<rowspan type="top">品種</rowspan>',
+    '<colspan type="top">売上数量<br>（本）</colspan>',
+    '<colspan type="top">総重量<br>（kg）</colspan>',
+    '<colspan type="top">売上金額</colspan>',
+    '<colspan type="top">スプレッド</colspan>',
+    '<colspan type="top">粗利金額</colspan>',
+    '<colspan type="top">（運賃）</colspan>',
+    '<colspan type="top">構成比</colspan>',
+    '<colspan type="top">累積<br>構成比</colspan>',
+    '<colspan type="top">販価<br>円/kg</colspan>',
+    '<colspan type="top">運賃<br>/kg</colspan>',
+    '<colspan type="top">置場販価<br>/kg</colspan>',
+    '<colspan type="top">粗利率</colspan>',
+    '<colspan type="top">粗利益<br>/kg</colspan>',
+    '<colspan type="top">対策と方針</colspan>',
+    '<colspan type="top">目標<br>増数量</colspan>',
+    '<colspan type="top">達成時期</colspan>',
   ];
   // 通常の際のカラム設定
   gridVal.getGridColumns = function(sumFlg) {
     return [{
+        data: 'runk',
+        type: 'text',
+        readOnly: true,
+        renderer: colorRenderer
+      },
+      {
         data: 'name',
         type: 'text',
         readOnly: true,
         renderer: colorRenderer
       },
       {
-        data: 'size',
-        type: 'text',
-        readOnly: true,
-        renderer: colorRenderer
-      },
-      {
-        data: 'perHourPrd',
+        data: 'codelist',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer
       },
       {
-        data: 'salweight',
+        data: 'salnum',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer,
       },
       {
-        data: 'salUnitPrc',
+        data: 'salweight',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
@@ -140,95 +139,95 @@ jQuery.noConflict();
         renderer: monetaryRenderer
       },
       {
-        data: 'mtrweight',
+        data: 'salsplead',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer
       },
       {
-        data: 'mtrUnitPrc',
+        data: 'salprofit',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer
       },
       {
-        data: 'mtrprice',
+        data: 'brgprice',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer
       },
       {
-        data: 'losweight',
-        type: 'numeric',
-        format: '0,0',
-        readOnly: true,
-        renderer: monetaryRenderer
-      },
-      {
-        data: 'losprice',
-        type: 'numeric',
-        format: '0,0',
-        readOnly: true,
-        renderer: monetaryRenderer
-      },
-      {
-        data: 'losUnit',
+        data: 'salUnit',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: percentageRenderer
       },
       {
-        data: 'prdweight',
-        type: 'numeric',
-        format: '0,0',
-        readOnly: true,
-        renderer: monetaryRenderer
-      },
-      {
-        data: 'prdUnitPrc',
-        type: 'numeric',
-        format: '0,0',
-        readOnly: true,
-        renderer: monetaryRenderer
-      },
-      {
-        data: 'prdYieldThr',
+        data: 'plusUnit',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: percentageRenderer
       },
       {
-        data: 'prdYield',
+        data: 'perSalprice',
+        type: 'numeric',
+        format: '0,0',
+        readOnly: true,
+        renderer: monetaryRenderer
+      },
+      {
+        data: 'perBrgprice',
+        type: 'numeric',
+        format: '0,0',
+        readOnly: true,
+        renderer: monetaryRenderer
+      },
+      {
+        data: 'perStcprice',
+        type: 'numeric',
+        format: '0,0',
+        readOnly: true,
+        renderer: monetaryRenderer
+      },
+      {
+        data: 'perProfit',
+        type: 'numeric',
+        format: '0,0',
+        readOnly: true,
+        renderer: percentageRenderer
+      },
+      {
+        data: 'perPrfprice',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: percentageRenderer,
       },
       {
-        data: 'spreadPrice',
+        data: 'solveComment',
+        type: 'text',
+        format: '0,0',
+        readOnly: true,
+        renderer: colorRenderer
+      },
+      {
+        data: 'goalNum',
         type: 'numeric',
         format: '0,0',
         readOnly: true,
         renderer: monetaryRenderer
       },
       {
-        data: 'spreadPer',
-        type: 'numeric',
+        data: 'goalPeriod',
+        type: 'text',
         format: '0,0',
         readOnly: true,
-        renderer: percentageRenderer
-      },
-      {
-        data: 'perHourPrf',
-        type: 'numeric',
-        format: '0,0',
-        readOnly: true,
-        renderer: monetaryRenderer
+        renderer: colorRenderer
       },
     ];
   };
@@ -247,11 +246,6 @@ jQuery.noConflict();
     // ユーザの設定値(期間)の読み込み
     let period = func.getPeriodFromTo();
     let err = [];
-    /** if (!func.checkPeriod(period, err)) {
-      alert(err);
-      spinner.hideSpinner();
-      return;
-    } */
     // 期間をsessionStorageに保存
     sessionStorage.setItem(val.SELECT_PERIOD_YEARMONTH, func.makeStorageYearMonth(period));
     // データの取得
@@ -265,7 +259,7 @@ jQuery.noConflict();
       $('#my-grid').empty();
       $('#total-grid').empty();
       // グリッドを表示
-      func.showPerhourListGrid(period);
+      func.showCstSalesRunkGrid(period);
       spinner.hideSpinner();
     }).catch(function(error) {
       alert('システムエラーが発生しました');

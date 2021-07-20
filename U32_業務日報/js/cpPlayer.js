96e64cdcb4d832cf5f28386c3f621102eb5bf8e4
Naoki <n.takahara@mdsj.jp>
2021-07-20 16:02:04 +0900

    完工となった工事依頼を日報で使えないように制御

--- U32_業務日報/js/cpPlayer.js
+++ U32_業務日報/js/cpPlayer.js
@@ -10,19 +10,45 @@
     'app.record.edit.submit'
   ];
   kintone.events.on(cfevents, function(event) {
-    var res = confirm("本当にこの内容を反映させますか");
-    if (res === false) {
-      event.error = "キャンセルしました";
-      window.location.href = window.location.origin + window.location.pathname + "#record=" + event.record.$id.value; // 画面遷移
+    var record = event.record;
+    var table = record['作業者一覧'].value;
+    var plList = '';
+    for (var i = 0; i < table.length; i++) {
+      if (plList !== '') {
+        plList += ', ';
+      }
+      plList += table[i].value['作業者名'].value;
     }
-    return event;
+    record['作業者リスト']['value'] = plList;
+    record['作業者リスト']['disabled'] = true;
+    // 完工済みの工事依頼かどうかチェック
+    var clientRecordId = event.record.工事番号.value;
+    var appId = kintone.app.getId();
+    var query = '工事番号 = ' + clientRecordId + ' and 完工区分 in ("完")';
+    var outputFields = ['日付'];
+    var appUrl = kintone.api.url('/k/v1/records');
+    var params = {
+      'app': appId,
+      'query': query,
+      'fields': outputFields
+    };
+    return kintone.api(appUrl, 'GET', params).then(function(resp) {
+      if (resp.records.length > 0) {
+        event.error = '既に完工済みの工事依頼は使用できません。';
+      } else {
+        var res = confirm("本当にこの内容を反映させますか");
+        if (res === false) {
+          event.error = 'キャンセルしました';
+          window.location.href = window.location.origin + window.location.pathname + "#record=" + event.record.$id.value; // 画面遷移
+        }
+      }
+      return event;
+    });
   });
 
   var events = [
     'app.record.create.show',
     'app.record.edit.show',
-    'app.record.create.submit',
-    'app.record.edit.submit',
     'app.record.create.change.作業者一覧',
     'app.record.edit.change.作業者一覧',
     'app.record.create.change.作業者名',

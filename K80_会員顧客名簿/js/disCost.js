0118789ec1c25971341274b18a507ff465035d85
Naoki <n.takahara@mdsj.jp>
2021-10-25 14:52:54 +0900

    入会金・保証金非表示対応

---
+++ K80_会員顧客名簿/js/disCost.js
@@ -0,0 +1,20 @@
+/*
+ * プラン入会金・保証金非表示制御
+ * 20211025 MDSJ takahara
+ */
+(function() {
+  'use strict';
+
+  var events = [
+    'app.record.detail.show',
+    'app.record.create.show',
+    'app.record.edit.show'
+  ];
+  kintone.events.on(events, function(event) {
+    var record = event.record;
+    kintone.app.record.setFieldShown('入会金', false);
+    kintone.app.record.setFieldShown('保証金', false);
+    kintone.app.record.setFieldShown('請求済', false);
+    return event;
+  });
+})();

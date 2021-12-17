d9c540e2a674929e8edc59f6f46c8ef0111c38e8
MurayamaTakahiko <murayama@wing-j.co.jp>
2021-12-17 11:40:02 +0900

    ウエマツ様請求日編集不可制御を外す

--- U31‗工事依頼/js/mb_chkComp.js
+++ U31‗工事依頼/js/mb_chkComp.js
@@ -91,7 +91,7 @@
         record['完工チェック'].value[0] = chkComp;
       }
       record['完工チェック']['disabled'] = true;
-      record['請求日']['disabled'] = true;
+      //record['請求日']['disabled'] = true;
       record['請求番号']['disabled'] = true;
       kintone.mobile.app.record.set(event);
     });

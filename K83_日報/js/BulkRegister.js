91f1af1c38bb4a736346ed77bb04a70bb05ac8a1
murayama <murayama@wing-j.co.jp>
2022-02-25 21:07:59 +0900

    共栄商事様日報

--- K83_日報/js/BulkRegister.js
+++ K83_日報/js/BulkRegister.js
@@ -17,11 +17,13 @@
     });
 
     //一括登録ボタン処理
-    $(document).on('click', '#bulk_button', function(ev) {
+    $(document).on('click', '#bulk_button', async (ev) => {
+      try {
       var result = window.confirm('一括登録しますか？');
       if(result==false){
           return;
       }
+      showSpinner(); // スピナー表示
       //アプリID
       var APP_ID = 445;   //日報
       var APP_SALES_ID = 446;
@@ -40,8 +42,9 @@
         'app': kintone.app.getId(),
         'query': '日時 >= "' + minDt +  '" and 日時 <="' + maxDt  + '" and ドロップダウン in ("ビジター利用日報","メンバー対応日報") and 売上登録済み = ""  order by 日時 '
       };
+
       //指定年月の日報を取得
-      proc= kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', body).then(function(resp) {
+      const resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', body);
         // success
         console.log(resp);
         var rec=resp.records;
@@ -71,15 +74,21 @@
 
           for(let j = 0 ; j<subrec.length ; j++){
             //自動計上項目が入力されているかつ、集金額1円以上
-            if(subrec[j]['value']['支払区分'].value =='支払済' && subrec[j]['value']['自動計上項目'].value != null && subrec[j]['value']['自動計上項目'].value !='' &&
+            if(subrec[j]['value']['支払区分'].value =='支払済' && subrec[j]['value']['自動計上項目'].value != null  &&
               subrec[j]['value']['集金額税込・自動計算'].value >= 1 ){
+              var item;
+              if(subrec[j]['value']['商品名'].value==null || subrec[j]['value']['商品名'].value==''){
+                item=subrec[j]['value']['自動計上項目'].value;
+              }else {
+                item=subrec[j]['value']['商品名'].value;
+              }
               insbody.record.売上明細.value.push({
                               "value":{
                                 "請求対象月":{
                                   "value":moment(rec[i]['日時'].value).format('YYYY-MM-DD')
                                 },
                                 "項目":{
-                                  "value":subrec[j]['value']['商品名'].value
+                                  "value":item
                                 },
                                 "金額":{
                                   "value":subrec[j]['value']['集金額税込・自動計算'].value
@@ -93,7 +102,7 @@
             }
             if (insbody.record.売上明細.value.length>0){
               //登録
-              return kintone.api(kintone.api.url('/k/v1/record.json', true), 'POST', insbody).then(function(resp) {
+              const resp2 = await kintone.api(kintone.api.url('/k/v1/record.json', true), 'POST', insbody);
                 var updbody={
                   "app":APP_ID,
                   "id":id,
@@ -103,34 +112,78 @@
                       }
                     }
                   };
-                return kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', updbody, function(resp) {
-                }, function(err) {
-                  // error
-                  console.log(err);
-                  throw err;
-                });
-              }).catch(function(err) {
-                // error
-                console.log(err);
-                throw err;
-              });
-              }
+                    await  kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', updbody).then(function(resp3) {
+                  });
 
-            }
-      }).catch(function(err) {
-        // error
-        console.log(err);
-        throw err;
-      });
-      //処理結果
-      proc.then(
-        function(result) {
-          alert('登録しました。');
-        },
-        function(error) {
-          alert(error.message);
-        }
-      );
+          }
 
+      }
+      hideSpinner(); // スピナー非表示
+      alert('登録しました。');
+      return ev;
+    } catch(e) {
+      // パラメータが間違っているなどAPI実行時にエラーが発生した場合
+      hideSpinner(); // スピナー非表示
+      alert(e.message);
+      return ev;
+    }
     });
+
+
+    // スピナーを動作させる関数
+    function showSpinner() {
+        // 要素作成等初期化処理
+        if ($('.kintone-spinner').length == 0) {
+            // スピナー設置用要素と背景要素の作成
+            var spin_div = $('<div id ="kintone-spin" class="kintone-spinner"></div>');
+            var spin_bg_div = $('<div id ="kintone-spin-bg" class="kintone-spinner"></div>');
+
+            // スピナー用要素をbodyにappend
+            $(document.body).append(spin_div, spin_bg_div);
+
+            // スピナー動作に伴うスタイル設定
+            $(spin_div).css({
+                'position': 'fixed',
+                'top': '50%',
+                'left': '50%',
+                'z-index': '510',
+                'background-color': '#fff',
+                'padding': '26px',
+                '-moz-border-radius': '4px',
+                '-webkit-border-radius': '4px',
+                'border-radius': '4px'
+            });
+
+            $(spin_bg_div).css({
+                'position': 'fixed',
+                'top': '0px',
+                'left': '0px',
+                'z-index': '500',
+                'width': '100%',
+                'height': '200%',
+                'background-color': '#000',
+                'opacity': '0.5',
+                'filter': 'alpha(opacity=50)',
+                '-ms-filter': "alpha(opacity=50)"
+            });
+
+            // スピナーに対するオプション設定
+            var opts = {
+                'color': '#000'
+            };
+
+            // スピナーを作動
+            new Spinner(opts).spin(document.getElementById('kintone-spin'));
+        }
+
+        // スピナー始動（表示）
+        $('.kintone-spinner').show();
+    }
+
+    // スピナーを停止させる関数
+    function hideSpinner() {
+        // スピナー停止（非表示）
+        $('.kintone-spinner').hide();
+    }
+
 })(jQuery);
